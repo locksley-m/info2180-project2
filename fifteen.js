a365bc68dff9a8156f4e7c3c6321bd0b8a915eea
Locksley Murray <loxli@live.com>
2016-10-31 22:14:13 -0500

    Dynamic puzzle with animation

--- fifteen.js
+++ fifteen.js
@@ -1,86 +1,134 @@
-var BLANK_X = 300;
-var BLANK_Y = 300;
-var	tiles=[];
+var blankXcord= "294px" ;// Location of the blank piece 
+var blankYcord= "294px";
+var base;
 
-window.onload=function()
-{
-	
-	tiles= document.querySelectorAll("#puzzlearea > div");
-	createTiles();
-	
-	
-   }
-	
 
-function createTiles(){		
-		  
-		var j=0,t=3;		
-		
-		for (var i = 0; i < tiles.length; i++) {
-			for (var x = 0; x <= t; x++) {
-				tiles[i].className+=" puzzlepiece"; // Assigning class attributes of puzzle piece
-                tiles[i].style.top = 100 * j + "px"; // Setting the location of the tile by row and columns 
-				tiles[i].style.left = 100 * x  + "px";
-				tiles[i].style.backgroundPosition = -x * 100 + "px " + j * -100 + "px";//Set image background position
-			    tiles[i].addEventListener("click" ,moveTile(tiles[i]));
-                tiles[i].addEventListener("mouseover", hover(tiles[i]));
+$(document).ready(function(){
+	
+	
+	
+	setTiles();
+	$("#shufflebutton").on("click",shuffle())
+	setMovable();
+	moveTiles();
+	
+	
+});
+
+
+function setTiles(){
+	var i = 0;
+		var j = 0;
+		var x = 0;
+		var y = 0;
+		$("#puzzlearea>div").each(function(){
+			$(this).addClass("puzzlepiece"); // Give each puzzle piece the attributes of puzzle piece
+			base = $("#puzzlearea>div:first-child").position();
+			x = base.left+(98*i);
+			y = base.top+(98*j);
+			$(this).css({ // Set location of the pieces
+				"top": y,
+				"left": x			
+				
+			});
+			
+			if(i<3){
 				i++;
+			}else{
+				i=0;	
+				j++;
 			}
-         j++;
-         if (j > 2) {
-		    t = 2;
-		 }+
-         i--;
-       }
+			$(this).css({   // Set background for each puzzle piece 
+				"background-position-x":0-x,
+				"background-position-y":+0-y
+			});
+			
+		
+		
+		
+	});
+}
+
+
+//Function to check if a tile neighbours the empty space
+function isNeighbor(x,y){ 
+	
+	
+	if ( Math.abs(parseInt(blankXcord) - parseInt(x)) == 98){
+       if (Math.abs(parseInt(blankYcord)- parseInt(y)) == 0){		
+	      console.log("True");
+		  return true;
+		  
+	   }
+	}else if( Math.abs(parseInt(blankYcord) - parseInt(y)) == 98){
+       if (Math.abs(parseInt(blankXcord)- parseInt(x)) == 0){		
+	       console.log("True"); 
+		   return true;
+	   }
+	}	
+	console.log("False");
+	return false;
+
+}
+
+//Set the tiles as movable piece or static
+function setMovable(){	
+    
+	$(".puzzlepiece").each(function(){
+     
+	 if( isNeighbor( $(this).css("left"), $(this).css("top")))		
+    	 $(this).addClass("movablepiece");
+	 
+	 else
+		 $(this).removeClass("movablepiece");
+	});
+	
+		
+}
+
+
+// Move tile to empty space if is movable
+function moveTiles(){
+	
+	var tempX;
+	var tempY;	
+	
+	$(".puzzlepiece").each(function(){
+	
+	$(this).off("click");	
+	
+	$(this).on("click", function(){	
+	if ( isNeighbor( $(this).css("left"), $(this).css("top")) ){	
+		tempX=$(this).css("left");
+		
+		tempY=$(this).css("top");
+		
+		$(this).animate({
+		   "top":blankYcord,
+		   "left":blankXcord			
+		   });
+		   
+		blankXcord=tempX;
+		blankYcord=tempY;	
+    	
+		setMovable();
+	    $(this).addClass("movablepiece");
 	}	
 	
-function hover(tile) {
-  if (neighborTest(tile.style.left, tile.style.top)) {
-    tile.className+=" movablepiece";
-  } else if (tile.className=="movablepiece") {
-    tile.removeAttribute("movablepiece");
-  }
+	console.log(blankXcord,blankYcord)
+	 });   
+   
+  });
+  
 }
 
 
-function moveTile(tile) {
+function shuffle(){
 	
-  if (neighborTest(tile.style.left, tile.style.top)) {
-    var tempX = tile.style.left;
-    var tempY = tile.style.top;
-    tile.style.left = BLANK_X + "px";
-    tile.style.top = BLANK_Y + "px";
-    BLANK_X = parseInt(tempX);
-    BLANK_Y = parseInt(tempY);
-  }
-	
+	$("#puzzlepiece").each(function(){
+		/*$(this).css({ 
+		  "top": ,
+		  "left": 						
+	  });*/
+	});
 }
-
-
-function neighborTest(x, y) {
-  if (Math.abs(BLANK_Y - parseInt(y)) == 100) {
-    if (Math.abs(BLANK_X - parseInt(x)) == 0) {
-      return true;
-    }
-  } else if (Math.abs(BLANK_X - parseInt(x)) == 100) {
-    if (Math.abs(BLANK_Y - parseInt(y)) == 0) {
-      return true;
-    }
-  }
-  return false;
-}
-
-// Shuffles the tiles by simulating 200 puzzle moves randomly
-function shuffle() {
-  var temp = [];
-  for (var i = 0; i < 200; i++) {
-    for (var j = 0; j < tiles.length; j++) {
-      if (neighborTest(tiles[j].style.left, tiles[j].style.top)) {
-        temp.push(tiles[j]);
-      }
-    }
-    moveTileHelp(temp[Math.floor(Math.random() * temp.length)]);
-    temp = [];
-  }
-}
-
